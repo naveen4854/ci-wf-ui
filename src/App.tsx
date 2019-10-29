d6f851342ba936a2e6bab0ecadf71e77415c2cc7
naresh.bairam <naresh.bairam@ggktech.com>
2019-10-29 19:01:21 +0530

    added sample projects details table

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './App.css';
 import SimpleMap from './sample-map';
 import PieSample from './sample-pie';
+import TableSample from './sample-table';
 
 const App: React.FC = () => {
   const [state, setState] = useState({
@@ -16,11 +17,14 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <div>
-        <div style={{ height: '50vh', width: '50vw', padding: 20, float: "left" }}>
+        <div style={{ height: '80vh', width: '50vw', padding: 20, float: "left" }}>
           <SimpleMap onCountrySelected={countrySelected}></SimpleMap>
         </div>
-        <div style={{ height: '50vh', width: '30vw', padding: 20, float: "right" }}>
+        <div style={{ height: '50vh', width: '45vw', padding: 20, float: "right" }}>
           <PieSample header={state.selectedCountry}/>
+        </div>
+        <div style={{ margin: '50px'}}>
+          <TableSample header={state.selectedCountry}/>
         </div>
       </div>
     </div>
---
+++ src/sample-table.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Table } from 'react-bootstrap';
+
+interface TEST {
+    header: string
+}
+
+const TableSample: React.FC<TEST> = ({ header }) => {
+    return (
+        <div>
+            <div>Project Details</div>
+            <Table responsive bordered>
+                <thead>
+                    <tr>
+                        <th>Survey Tool</th>
+                        <th># Completed</th>
+                        <th># Live Projects</th>
+                        <th># Live Offline</th>
+                        <th># Live Online</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Survey To Go</td>
+                        <td>125</td>
+                        <td>30</td>
+                        <td>25</td>
+                        <td>5</td>
+                    </tr>
+                    <tr>
+                        <td>ConfirmIT</td>
+                        <td>804</td>
+                        <td>50</td>
+                        <td>42</td>
+                        <td>8</td>
+                    </tr>
+                    <tr>
+                        <td>Decipher</td>
+                        <td>66</td>
+                        <td>4</td>
+                        <td>0</td>
+                        <td>4</td>
+                    </tr>
+                    <tr>
+                        <td>Total</td>
+                        <td>995</td>
+                        <td>84</td>
+                        <td>2710</td>
+                        <td>680</td>
+                    </tr>
+                </tbody>
+            </Table>
+        </div>
+    )
+}
+
+export default TableSample;
+
