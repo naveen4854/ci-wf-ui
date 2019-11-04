6d8d199cb236f20f3f8adfeb27bf38fd9179ce1e
Naveen Kuthadi <naveen.kuthadi@ggktech.com>
2019-11-04 12:08:38 +0530

    pertsample

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,15 +1,17 @@
 import React from "react"
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import { App, Header, Footer, ProgressTracker } from "../components/index";
+import PertSample from "../components/pert-sample/pert-sample";
 
 const Routes = () => {
   return (
     <Router>
       <Route component={Header} />
       <Switch>
-        <Route exact path="/" component={App} />
+        <Route exact path="/" component={PertSample} />
         <Route exact path="/dashboard" component={App} />
         <Route exact path="/progress-tracker" component={ProgressTracker} />
+        <Route exact path="/p" component={PertSample} />
       </Switch>
       <Route component={Footer} />
     </Router>
