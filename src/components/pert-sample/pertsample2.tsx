1a60ffeea4bf9d12743b47d925d7ad699e8f81d7
Naveen Kuthadi <naveen.kuthadi@ggktech.com>
2019-11-05 12:48:08 +0530

    text sample

--- src/components/pert-sample/pertsample2.tsx
+++ src/components/pert-sample/pertsample2.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Text = () => {
-    return <text x={0} y={105} fill="black">I love SasdVG!</text>
+    return <text x={0} y={505} fill="black">I love SasdVG!</text>
 }
 
 const data = [
@@ -48,14 +48,22 @@ const data = [
 ]
 
 const renderRect = (rectWidth: number, rectHeight: number, rectX: number, rectY: number) => {
-    return <rect width={rectWidth} height={rectHeight} x={rectX} y={rectY} rx={5} ry={5} style={{ fill: 'cornflowerblue', stroke: 'black', strokeWidth: 2, opacity: 0.6 }} />;
+    return <svg x={rectX} y={rectY} fill='pink' >
+        {/* <rect width={rectWidth} height={rectHeight}></rect> */}
+        <rect width={rectWidth} height={rectHeight / 3} x={2} y={2}
+            style={{ fill: '#0069d9', stroke: 'black', strokeWidth: 2, opacity: 1 }} />
+        <rect width={rectWidth} height={2 * rectHeight / 3} x={2} y={rectHeight / 3 + 2}
+            style={{ fill: 'white', stroke: 'black', strokeWidth: 2, opacity: 1 }} />
+        <text x={10} y={50} overflow="hidden" fill="black">I love SasdVG!</text>
+        <text x={10} y={150} overflow="hidden" fill="black">I love SasdVG!</text>
+    </svg >;
 }
 
 const PertSample2: React.FC = () => {
-    const rectwidth = 150, rectheight = 100;
+    const rectwidth = 250, rectheight = 150, gapX = 100, gapY = 100;
     let count = 0, value = data[0].categoryId, defx = 0, defy = 0;
     return (
-        <div style={{ margin: 100, height: '100vh' }}>
+        <div style={{ margin: 0, height: '100vh' }}>
             <svg width="auto" height="auto">
                 {data.map((d, index) => {
                     {
@@ -65,8 +73,8 @@ const PertSample2: React.FC = () => {
                             count = 1;
                             value = d.categoryId;
                         }
-                        defx = (rectwidth * d.categoryId) + 50 * (d.categoryId - 1);
-                        defy = 50 * (count - 1) + (rectheight * count);
+                        defx = (rectwidth * d.categoryId) + gapX * (d.categoryId - 1);
+                        defy = gapY * (count - 1) + (rectheight * count);
                         d.x = defx; d.y = defy;
                         return <g>
                             {renderRect(rectwidth, rectheight, defx, defy)}
@@ -82,11 +90,12 @@ const PertSample2: React.FC = () => {
                         const parent = data.find(p => p.id === d.pId);
                         if (!parent)
                             return <React.Fragment></React.Fragment>
-                        const parentRight = { x: parent.x + rectwidth, y: parent.y + rectheight / 2 }
-                        const childLeft = { x: child.x, y: child.y + rectheight / 2 }
+                        const parentRight = { x: parent.x + rectwidth + 2, y: parent.y + 2 + rectheight / 2 }
+                        const childLeft = { x: child.x + 2, y: child.y + 2 + rectheight / 2 }
                         return <line x1={parentRight.x} y1={parentRight.y} x2={childLeft.x} y2={childLeft.y} style={{ stroke: 'black', strokeWidth: 2 }} />
                     })
                 }
+
             </svg>
         </div>
     )
