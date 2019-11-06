773ac5852915af5154285e410e37e0bd1f6d72c2
Naveen Kuthadi <naveen.kuthadi@ggktech.com>
2019-11-06 13:36:46 +0530

    data moved out

---
+++ src/components/pert-sample/data.ts
@@ -0,0 +1,88 @@
+const data = [
+    {
+        id: 0,
+        stage: "Commissioned",
+        categoryId: 1,
+        pId: null,
+        x: 0,
+        y: 0,
+        text: {
+            startDate: "29-3-1993",
+            endDate: "5-3-2001"
+        }
+    },
+    {
+        id: 1,
+        stage: "Programming - STG",
+        categoryId: 2,
+        pId: [0],
+        x: 0,
+        y: 0,
+        text: {
+            startDate: "29-3-1993",
+            endDate: "5-3-2001"
+        }
+    },
+    {
+        id: 2,
+        stage: "Programming - ConfirmIt",
+        categoryId: 2,
+        pId: [0],
+        x: 0,
+        y: 0,
+        text: {
+            startDate: "29-3-1993",
+            endDate: "5-3-2001"
+        }
+    },
+    {
+        id: 3,
+        stage: "Collection - STG",
+        categoryId: 3,
+        pId: [1],
+        x: 0,
+        y: 0,
+        text: {
+            startDate: "29-3-1993",
+            endDate: "5-3-2001"
+        }
+    },
+    {
+        id: 4,
+        stage: "Collection- ConfirmIt",
+        categoryId: 3,
+        pId: [2],
+        x: 0,
+        y: 0,
+        text: {
+            startDate: "29-3-1993",
+            endDate: "5-3-2001"
+        }
+    },
+    {
+        id: 5,
+        stage: "Collection- ConfirmIt",
+        categoryId: 3,
+        pId: [2],
+        x: 0,
+        y: 0,
+        text: {
+            startDate: "29-3-1993",
+            endDate: "5-3-2001"
+        }
+    },
+    {
+        id: 6,
+        stage: "Done - ConfirmIt",
+        categoryId: 4,
+        pId: [3, 4, 5],
+        x: 0,
+        y: 0,
+        text: {
+            startDate: "29-3-1993",
+            endDate: "5-3-2001"
+        }
+    }
+]
+
+export default data;
