3ae853732be1f1c7a625ec0761c7f6226f3ebb9f
Naveen Kuthadi <naveen.kuthadi@ggktech.com>
2019-11-06 13:29:27 +0530

    arrows dynamic

--- src/components/pert-sample/pertsample2.tsx
+++ src/components/pert-sample/pertsample2.tsx
@@ -20,9 +20,9 @@ const data = [
     },
     {
         id: 1,
-        stage: "Programming",
+        stage: "Programming - STG",
         categoryId: 2,
-        pId: 0,
+        pId: [0],
         x: 0,
         y: 0,
         text: {
@@ -32,9 +32,9 @@ const data = [
     },
     {
         id: 2,
-        stage: "Programming",
+        stage: "Programming - ConfirmIt",
         categoryId: 2,
-        pId: 0,
+        pId: [0],
         x: 0,
         y: 0,
         text: {
@@ -44,9 +44,9 @@ const data = [
     },
     {
         id: 3,
-        stage: "Collection",
+        stage: "Collection - STG",
         categoryId: 3,
-        pId: 1,
+        pId: [1],
         x: 0,
         y: 0,
         text: {
@@ -56,9 +56,33 @@ const data = [
     },
     {
         id: 4,
-        stage: "Collection",
+        stage: "Collection- ConfirmIt",
         categoryId: 3,
-        pId: 2,
+        pId: [2],
+        x: 0,
+        y: 0,
+        text: {
+            startDate: "29-3-1993",
+            endDate: "5-3-2001"
+        }
+    },
+    {
+        id: 5,
+        stage: "Collection- ConfirmIt",
+        categoryId: 3,
+        pId: [2],
+        x: 0,
+        y: 0,
+        text: {
+            startDate: "29-3-1993",
+            endDate: "5-3-2001"
+        }
+    },
+    {
+        id: 6,
+        stage: "Done - ConfirmIt",
+        categoryId: 4,
+        pId: [3, 4, 5],
         x: 0,
         y: 0,
         text: {
@@ -69,12 +93,13 @@ const data = [
 ]
 
 const renderRect = (rectWidth: number, rectHeight: number, rectX: number, rectY: number, text: any, stage: string) => {
+    const strokeWidth = 2;
     return <svg x={rectX} y={rectY} fill='pink' >
-        {/* <rect width={rectWidth} height={rectHeight}></rect> */}
-        <rect width={rectWidth} height={rectHeight / 3} x={2} y={2}
-            style={{ fill: '#0069d9', stroke: 'black', strokeWidth: 2, opacity: 1 }} />
-        <rect width={rectWidth} height={2 * rectHeight / 3} x={2} y={rectHeight / 3 + 2}
-            style={{ fill: 'white', stroke: 'black', strokeWidth: 2, opacity: 1 }} />
+        <rect width={rectWidth} height={rectHeight}></rect>
+        <rect onClick={() => alert('hey')} width={rectWidth - strokeWidth} height={rectHeight / 3 - strokeWidth} x={strokeWidth} y={strokeWidth}
+            style={{ fill: '#0069d9', stroke: 'black', strokeWidth: strokeWidth, opacity: 1 }} />
+        <rect width={rectWidth - strokeWidth} height={2 * rectHeight / 3 - strokeWidth} x={strokeWidth} y={rectHeight / 3}
+            style={{ fill: 'white', stroke: 'black', strokeWidth: strokeWidth, opacity: 1 }} />
         <text x={10} y={30} overflow="hidden" fill="white">{stage}</text>
         <text x={10} y={100} overflow="hidden" fill="black">Start Date: {text.startDate}</text>
         <text x={10} y={120} overflow="hidden" fill="black">End Date: {text.endDate}</text>
@@ -82,30 +107,55 @@ const renderRect = (rectWidth: number, rectHeight: number, rectX: number, rectY:
 }
 
 const PertSample2: React.FC = () => {
-    const rectwidth = 250, rectheight = 150, gapX = 100, gapY = 100;
+    const rectwidth = 250, rectheight = 150, gapX = 200, gapY = 100;
     let count = 0, value = data[0].categoryId, defx = 0, defy = 0, text = null;
+
+    const getTargetPoint = (childRect: any, parentRight: any) => {
+        const childLeft = { x: childRect.x - 10, y: childRect.y + rectheight / 2 }
+        const childTop = { x: childRect.x + rectwidth / 3, y: childRect.y - 8 }
+        const childBottom = { x: childRect.x + rectwidth / 5, y: childRect.y + rectheight + 8 }
+        // const childRight = { x: childRect.x + rectwidth, y: childRect.y + rectheight / 3 };
+
+        const childLeftDist = distance(parentRight, childLeft);
+        const childTopDist = distance(parentRight, childTop);
+        const childBottomDist = distance(parentRight, childBottom);
+        console.log('childLeftDist', childLeftDist, 'childTopDist', childTopDist, 'childBottomDist', childBottomDist, 'xx');
+
+        if (childLeftDist < childTopDist) {
+            return childLeftDist < childBottomDist ? childLeft : childBottom;
+        } else {
+            return childTopDist < childBottomDist ? childTop : childBottom;
+        }
+
+        // let target = parentRight.y === childRect.y ? childLeft : childTop;
+        // return target;
+    }
+
+    const distance = (p1: { x: number, y: number }, p2: { x: number, y: number }) => {
+        return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
+    }
+
     return (
-        <div style={{ margin: 0, height: '100vh' }}>
-            <svg width="auto" height="auto">
-                {data.map((d, index) => {
+        <div style={{ margin: 0, height: '100vh', width: '100vw', overflow: 'scroll' }}>
+            <svg width="200vw" height="200vh" overflow='visible'>
+                {
+                    data.map((d, index) => {
 
-                    if (d.categoryId == value) {
-                        count++
-                    } else {
-                        count = 1;
-                        value = d.categoryId;
-                    }
-                    defx = (rectwidth * d.categoryId) + gapX * (d.categoryId - 1);
-                    defy = gapY * (count - 1) + (rectheight * count);
-                    text = d.text;
-                    d.x = defx; d.y = defy;
-                    return <g>
-                        {renderRect(rectwidth, rectheight, defx, defy, text, d.stage)}
-                    </g>
-
-
+                        if (d.categoryId == value) {
+                            count++
+                        } else {
+                            count = 1;
+                            value = d.categoryId;
+                        }
+                        defx = (rectwidth * d.categoryId) + gapX * (d.categoryId - 1);
+                        defy = gapY * (count - 1) + (rectheight * count);
+                        text = d.text;
+                        d.x = defx; d.y = defy;
+                        return <g>
+                            {renderRect(rectwidth, rectheight, defx, defy, text, d.stage)}
+                        </g>
+                    })
                 }
-                )}
                 <defs>
                     <marker id="startarrow" markerWidth="10" markerHeight="7"
                         refX="10" refY="3.5" orient="auto">
@@ -121,20 +171,19 @@ const PertSample2: React.FC = () => {
                         if (d.pId === null)
                             return <React.Fragment></React.Fragment>
                         const child = d;
-                        const parent = data.find(p => p.id === d.pId);
-                        if (!parent)
-                            return <React.Fragment></React.Fragment>
-                        const parentRight = { x: parent.x + rectwidth + 2, y: parent.y + 2 + rectheight / 2 }
-                        const childLeft = { x: child.x + 2 - 10, y: child.y + 2 + rectheight / 2 }
-                        const childTop = { x: child.x + 2 + rectwidth / 3, y: child.y + 2 - 8 }
-                        console.log(parent, child, parent.x, child.x, 'xx')
-                        let target = parent.y === child.y ? childLeft : childTop;
+                        return d.pId.map((p) => {
+                            const parent = data.find(a => a.id === p);
+                            if (!parent)
+                                return <React.Fragment></React.Fragment>
+                            const parentRight = { x: parent.x + rectwidth + 2, y: parent.y + 2 + rectheight / 2 }
+                            let target = getTargetPoint(child, parent);
 
-                        return <g>
+                            return <g>
 
-                            <line x1={parentRight.x} y1={parentRight.y} markerEnd="url(#endarrow)"
-                                x2={target.x} y2={target.y} style={{ stroke: 'red', strokeWidth: 2 }} />
-                        </g>
+                                <line x1={parentRight.x} y1={parentRight.y} markerEnd="url(#endarrow)"
+                                    x2={target.x} y2={target.y} style={{ stroke: 'red', strokeWidth: 2 }} />
+                            </g>
+                        })
                     })
                 }
 
