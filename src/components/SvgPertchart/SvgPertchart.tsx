1c6d35fc18fde80760bdbc5bacf8c339771095c6
naresh.bairam <naresh.bairam@ggktech.com>
2019-11-07 17:27:59 +0530

    added stage name and legends

--- src/components/SvgPertchart/SvgPertchart.tsx
+++ src/components/SvgPertchart/SvgPertchart.tsx
@@ -7,9 +7,13 @@ const stages = [
         children: [
             {
                 id: 11,
+                stage: "Protrack",
                 parentId: [],
                 name: "stage1-child1",
-                first: true
+                first: true,
+                status: "completed",
+                startDate: "29-3-1993",
+                endDate: "5-3-2001",
             }
         ]
     },
@@ -19,14 +23,31 @@ const stages = [
         children: [
             {
                 id: 21,
+                stage: "STG",
                 name: "stage2-child1",
                 first: true,
-                parentId: [11]
+                parentId: [11],
+                status: "progress",
+                startDate: "29-3-1993",
+                endDate: "5-3-2001",
             },
             {
                 id: 22,
+                stage: "ConfirmIt",
                 name: "stage2-child2",
-                parentId: [11]
+                parentId: [11],
+                status: "completed",
+                startDate: "29-3-1993",
+                endDate: "5-3-2001",
+            },
+            {
+                id: 23,
+                stage: "Decipher",
+                name: "stage3-child3",
+                parentId: [11],
+                status: "delayed",
+                startDate: "29-3-1993",
+                endDate: "5-3-2001"
             }
         ]
     },
@@ -36,19 +57,31 @@ const stages = [
         children: [
             {
                 id: 31,
+                stage: "STG",
                 name: "stage3-child1",
                 first: true,
-                parentId: [21]
+                parentId: [21],
+                status: "completed",
+                startDate: "29-3-1993",
+                endDate: "5-3-2001",
             },
             {
                 id: 32,
+                stage: "ConfirmIt",
                 name: "stage3-child2",
-                parentId: [21]
+                parentId: [21],
+                status: "completed",
+                startDate: "29-3-1993",
+                endDate: "5-3-2001",
             },
             {
                 id: 33,
+                stage: "Decipher",
                 name: "stage3-child3",
-                parentId: [22]
+                parentId: [22],
+                status: "delayed",
+                startDate: "29-3-1993",
+                endDate: "5-3-2001",
             }
         ]
     },
@@ -59,10 +92,53 @@ const stages = [
             {
                 id: 41,
                 name: "stage4-child1",
+                stage: "ConfirmIt",
                 first: true,
-                parentId: [31, 32, 33]
+                parentId: [31, 32, 33],
+                status: "progress",
+                startDate: "29-3-1993",
+                endDate: "5-3-2001",
             }
         ]
+    },
+    {
+        id: 5,
+        name: "stage5",
+        children: [
+            {
+                id: 51,
+                name: "stage5-child1",
+                stage: "ConfirmIt",
+                first: true,
+                parentId: [41],
+                status: "progress",
+                startDate: "29-3-1993",
+                endDate: "5-3-2001",
+            }
+        ]
+    }
+]
+
+const stageNames = [
+    "Commissioned",
+    "Programming",
+    "Collection",
+    "QA Check",
+    "Delivery"
+]
+
+const legend = [
+    {
+        stage: "Completed",
+        color:  "#447733"
+    },
+    {
+        stage: "In Progress",
+        color:  "#FFBF00"
+    },
+    {
+        stage: "Delayed",
+        color:  "#DD0014"
     }
 ]
 
@@ -73,7 +149,8 @@ interface ISvgRectangle {
     height: number,
     stroke?: string,
     strokeWidth?: number,
-    fill?: string
+    fill?: string,
+    opacity?: number
 }
 
 interface ISvgLine {
@@ -107,12 +184,25 @@ const Line: React.FC<ISvgLine> = (props) => {
     )
 }
 
+const header = (header: string, x: number, y: number) => {
+    return <text id="headers" x={x} y={y} width={150} height={120} fill="black">{header}</text>
+}
+
+
+const colorLegend = (color: string, state: string, x: number, y: number) => {
+    return <g>
+        <rect width={20} height={10} x={x} y={y} style={{ fill: color }} />
+        <text x={x + 20} y={y} fill="black">{state}</text>
+    </g>
+}
+
 const SvgPertchart: React.FC = () => {
 
-    var dimension = { x: 0, y: 0, width: 100, height: 50, gap: 50 };
+    var dimension = { x: 0, y: 0, width: 150, height: 120, gap: 70 };
     const midPoints: Map<number, IPoint> = new Map();
 
     const getStages = () => {
+        dimension = { x: 0, y: 40, width: 150, height: 120, gap: 70 };
         return stages.map(stage => getChildren(stage.children));
     }
 
@@ -127,7 +217,7 @@ const SvgPertchart: React.FC = () => {
 
             if (index === 0) {
                 dimension.x = dimension.x + dimension.width + dimension.gap;
-                dimension.y = 0
+                dimension.y = 40
             } else {
                 dimension.y = dimension.y + dimension.height + dimension.gap;
             }
@@ -138,12 +228,32 @@ const SvgPertchart: React.FC = () => {
 
             midPoints.set(child.id, getMidpoint(x1, y1, x1, y2));
 
-            return (<Rectangle key={child.id} x={dimension.x} y={dimension.y} width={dimension.width} height={dimension.height} fill="lightblue" />)
+            let color;
+            if (child.status === "completed") {
+                color = '#447733'
+            } else if (child.status === "progress") {
+                color = '#FFBF00'
+            } else if (child.status === "delayed") {
+                color = '#DD0014';
+            }
+
+            return (
+                <React.Fragment key={child.id}>
+                    <Rectangle x={dimension.x} y={dimension.y} width={dimension.width}
+                        height={dimension.height / 3} fill={color} stroke='black' strokeWidth={2} opacity={1} />
+                    <Rectangle x={dimension.x} y={dimension.y + dimension.height / 3} width={dimension.width}
+                        height={2 * dimension.height / 3} fill='white' stroke='black' strokeWidth={2} opacity={1} />
+
+                    <text x={dimension.x + 10} y={dimension.y + 30} overflow="hidden" fill="white">{child.stage}</text>
+                    <text x={dimension.x + 10} y={dimension.y + 70} overflow="hidden" fill="black">Start Date: {child.startDate} </text>
+                    <text x={dimension.x + 10} y={dimension.y + 90} overflow="hidden" fill="black">End Date: {child.endDate}</text>
+                </React.Fragment>
+            );
         });
     }
 
     const getLineStages = () => {
-        dimension = { x: 0, y: 0, width: 100, height: 50, gap: 50 };
+        dimension = { x: 0, y: 0, width: 150, height: 120, gap: 70 };
         return stages.map(stage => getLineChildren(stage.children));
     }
 
@@ -152,7 +262,7 @@ const SvgPertchart: React.FC = () => {
 
             if (index === 0) {
                 dimension.x = dimension.x + dimension.width + dimension.gap;
-                dimension.y = 0
+                dimension.y = 40
             } else {
                 dimension.y = dimension.y + dimension.height + dimension.gap;
             }
@@ -167,12 +277,35 @@ const SvgPertchart: React.FC = () => {
         });
     }
 
+    const getStageNames = () => {
+        dimension = { x: 0, y: 0, width: 150, height: 100, gap: 70 }
+        return stageNames.map((name, index) => {
+            if (index === 0) {
+                dimension.x = dimension.x + 220;
+            } else {
+                dimension.x = dimension.x + dimension.width + dimension.gap;
+            }
+            return header(name, dimension.x, dimension.y + 20);
+        });
+    }
+
+    const getLegends = () => {
+        dimension = { x: 650, y: 0, width: 150, height: 100, gap: 100 }
+        return legend.map(legend => {
+            dimension.x = dimension.x + dimension.gap;
+            dimension.y = 650;
+            return colorLegend(legend.color, legend.stage, dimension.x, dimension.y);
+        })
+    }
+
     const style = { margin: 65 };
     return (
         <div style={style} className="svg-container">
-            <svg width="1000" height="500">
+            <svg width="1500" height="1500">
+                {getStageNames()}
                 {getStages()}
                 {getLineStages()}
+                {getLegends()}
             </svg>
         </div>
     )
